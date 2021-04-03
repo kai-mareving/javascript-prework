b644a367d0d763112a4db8126e37fdf3b3b93370
Kai Mareving <karska.joanna@gmail.com>
2021-04-03 17:12:39 +0200

    Add var randomNumber and call it

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 let computerMove = 'rock';
 let playerMove = 'paper';
 
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
 printMessage('I got ' + computerMove + '! If your move is' + playerMove + ', you win!');
+
+printMessage('randomNumber is ' + randomNumber);
