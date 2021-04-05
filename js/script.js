7391c096bb85503abd3ae289aa4a1cdb09af98c0
Kai Mareving <karska.joanna@gmail.com>
2021-04-05 16:48:11 +0200

    Start implementing different mechanics.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,39 @@
+// NEW FUNCTIONS
+function getMoveName(MoveId) {
+    if(MoveId == 1){
+      return 'rock';
+    } else if(MoveId == 2) {
+        return 'paper';
+    } else if (MoveId == 3) {
+        return 'scissors';
+    } else {
+        printMessage('Move with id ' + MoveId + ' is unknown.');
+        return 'unknown move';
+    }
+}
+  
+function displayResult(ComputerMove, PlayerMove){
+    printMessage('I played ' + ComputerMove + ', and you ' + PlayerMove);
+  
+    if( ComputerMove == 'rock' && PlayerMove == 'paper'){
+      printMessage('You win!');
+    } else {
+      printMessage('This time you loose :(');
+    }
+}
+/******* changes are not yet staged in git       */  
+
+
 // Computers turn
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-console.log('Drawn number is: ' + randomNumber);
+//console.log('Drawn number is: ' + randomNumber);
 
-let computerMove = 'unknown move';
+let computerMove = getMoveName(getRandomNumber);
+//let computerMove = 'unknown move';
+console.log(computerMove);
 
-if(randomNumber == 1){
+/* if(randomNumber == 1){
   computerMove = 'rock';
 } else if (randomNumber == 2) {
     computerMove = 'paper';
@@ -15,10 +43,10 @@ if(randomNumber == 1){
     printMessage('Something went wrong');
 }
 
-printMessage('My move is: ' + computerMove);
+printMessage('My move is: ' + computerMove); */
 
 // Players turn
-let playerInput = prompt('Choose your move! 1: rock, 2: paper, 3: scissors.');
+/* let playerInput = prompt('Choose your move! 1: rock, 2: paper, 3: scissors.');
 
 console.log('Player chose: ' + playerInput);
 
@@ -34,10 +62,10 @@ if(playerInput == '1'){
     printMessage('Something went wrong');
 }
 
-printMessage('Your move is: ' + playerMove);
+printMessage('Your move is: ' + playerMove); */
 
 // Game sum-up
-if (computerMove == 'rock' && playerMove == 'paper' || computerMove == 'paper' && playerMove == 'scissors' || computerMove == 'scissors' && playerMove == 'rock') {
+/* if (computerMove == 'rock' && playerMove == 'paper' || computerMove == 'paper' && playerMove == 'scissors' || computerMove == 'scissors' && playerMove == 'rock') {
     printMessage('You win!');
 } else if (computerMove === playerMove && playerMove !== 'unknown move' && computerMove !== 'unknown move') {
     printMessage('We have a tie');
@@ -45,4 +73,4 @@ if (computerMove == 'rock' && playerMove == 'paper' || computerMove == 'paper' &
     printMessage('Something went wrong');
 } else {
     printMessage('Computer won. You loose. We are sorry. Try again!');
-}
+} */
