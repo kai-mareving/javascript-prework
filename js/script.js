6242851890c7891ef38ec0310271208a133af36c
Kai Mareving <karska.joanna@gmail.com>
2021-04-06 16:02:27 +0200

    Change game mechanisc v1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-function playGame() {
-    
+'use strict';
+
+function playGame(gameInput) {
+
+    let playerInput = gameInput;
+
     function getMoveName(moveId) {
         if (moveId == 1) {
             return 'rock';
@@ -8,20 +12,20 @@ function playGame() {
         } else if (moveId == 3) {
             return 'scissors';
         } else {
-            printMessage('Move with id ' + moveId + ' is unknown.');
+            printMessage('Move with id ' + moveId + ' is unknown.<br><br>');
             return 'unknown move';
         }
     }
 
     function displayResult(computerMove, playerMove) {
-        printMessage('I played ' + computerMove + ', and you played ' + playerMove);
+        printMessage('Computer played: ' + computerMove + '<br> You played: ' + playerMove + '.' + '<br><br>');
   
         if (computerMove == 'rock' && playerMove == 'paper'
             || computerMove == 'paper' && playerMove == 'scissors'
             || computerMove == 'scissors' && playerMove == 'rock') {
             printMessage('You win!');
         } else if (computerMove === playerMove) {
-            printMessage('We have a tie');
+            printMessage('We have a tie!');
         } else if (playerMove == 'unknown move' || computerMove == 'unknown move') {
             printMessage('Something went wrong!');
         } else {
@@ -36,11 +40,14 @@ function playGame() {
     console.log('randomNumber: ' + randomNumber + '. computerMove: ' + computerMove);
 
     // Players turn --> this needs changing to button choice
-    let playerInput = prompt('Choose your move! 1: rock, 2: paper, 3: scissors.');
+    // let playerInput = prompt('Choose your move! 1: rock, 2: paper, 3: scissors.');
     let playerMove = getMoveName(playerInput);
 
-    console.log('playerMove: ' + playerMove);
+    console.log('Our initial gameInput saved in playerInput: ' + playerInput + '. And playerMove is: ' + playerMove);
 
-    // Action!
     displayResult(computerMove, playerMove);
 }
+
+
+// Action!
+playGame(3);
