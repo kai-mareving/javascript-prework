f85786d343dbeb858b25517d71aac965ddbcce4a
Kai Mareving <karska.joanna@gmail.com>
2021-04-06 03:46:09 +0200

    Implement final game mechanics.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// NEW FUNCTIONS
 function getMoveName(moveId) {
     if(moveId == 1){
       return 'rock';
@@ -11,64 +10,35 @@ function getMoveName(moveId) {
         return 'unknown move';
     }
 }
+
+// Game sum-up
+function displayResult(computerMove, playerMove) {
+    printMessage('I played ' + computerMove + ', and you played ' + playerMove);
   
-function displayResult(computerMove, playerMove){
-    printMessage('I played ' + computerMove + ', and you ' + playerMove);
-  
-    if( computerMove == 'rock' && playerMove == 'paper'){
-      printMessage('You win!');
+    if (computerMove == 'rock' && playerMove == 'paper'
+        || computerMove == 'paper' && playerMove == 'scissors'
+        || computerMove == 'scissors' && playerMove == 'rock') {
+        printMessage('You win!');
+    } else if (computerMove === playerMove) {
+        printMessage('We have a tie');
+    } else if (playerMove == 'unknown move' || computerMove == 'unknown move') {
+        printMessage('Something went wrong!');
     } else {
-      printMessage('This time you loose :(');
+        printMessage('This time you loose :(');
     }
 }
 
 // Computers turn
 let randomNumber = Math.floor(Math.random() * 3 + 1);
+let computerMove = getMoveName(randomNumber);
 
-//console.log('Drawn number is: ' + randomNumber);
-
-let computerMove = getMoveName(getRandomNumber);
-//let computerMove = 'unknown move';
-console.log(computerMove);
-
-/* if(randomNumber == 1){
-  computerMove = 'rock';
-} else if (randomNumber == 2) {
-    computerMove = 'paper';
-} else if (randomNumber == 3) {
-    computerMove = 'scissors';
-} else {
-    printMessage('Something went wrong');
-}
-
-printMessage('My move is: ' + computerMove); */
+console.log('randomNumber: ' + randomNumber + '. computerMove: ' + computerMove);
 
 // Players turn
-/* let playerInput = prompt('Choose your move! 1: rock, 2: paper, 3: scissors.');
+let playerInput = prompt('Choose your move! 1: rock, 2: paper, 3: scissors.');
+let playerMove = getMoveName(playerInput);
 
-console.log('Player chose: ' + playerInput);
+console.log('playerMove: ' + playerMove);
 
-let playerMove = 'unknown move';
-
-if(playerInput == '1'){
-  playerMove = 'rock';
-} else if (playerInput == '2') {
-    playerMove = 'paper';
-} else if (playerInput == '3') {
-    playerMove = 'scissors';
-} else {
-    printMessage('Something went wrong');
-}
-
-printMessage('Your move is: ' + playerMove); */
-
-// Game sum-up
-/* if (computerMove == 'rock' && playerMove == 'paper' || computerMove == 'paper' && playerMove == 'scissors' || computerMove == 'scissors' && playerMove == 'rock') {
-    printMessage('You win!');
-} else if (computerMove === playerMove && playerMove !== 'unknown move' && computerMove !== 'unknown move') {
-    printMessage('We have a tie');
-} else if (computerMove == 'unknown move' || playerMove == 'unknown move') {
-    printMessage('Something went wrong');
-} else {
-    printMessage('Computer won. You loose. We are sorry. Try again!');
-} */
+// Action!
+displayResult(computerMove,playerMove);
