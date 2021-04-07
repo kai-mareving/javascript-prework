74e920350f2152d0bac6240d2e5c5e21d698c7f6
Kai Mareving <karska.joanna@gmail.com>
2021-04-07 16:18:12 +0200

    Fix code: enclose entire game in const.

--- js/script.js
+++ js/script.js
@@ -1,48 +1,47 @@
 'use strict';
 
-function playGame(playerInput) {
+const playGame = function (playerInput) {
     clearMessages();
-
-    const getMoveName = function (moveId) {
-        if (moveId == 1) {
-            return 'rock';
-        } else if (moveId == 2) {
-            return 'paper';
-        } else if (moveId == 3) {
-            return 'scissors';
-        } else {
-            printMessage('Move with id ' + moveId + ' is unknown.<br><br>');
-            return 'unknown move';
-        }
-    };
-
-    const displayResult = function(computerMove, playerMove) {
-        printMessage('Computer played: ' + computerMove + '<br> You played: ' + playerMove + '.' + '<br><br>');
-  
-        if (computerMove == 'rock' && playerMove == 'paper'
-            || computerMove == 'paper' && playerMove == 'scissors'
-            || computerMove == 'scissors' && playerMove == 'rock') {
-            printMessage('You win!');
-        } else if (computerMove === playerMove) {
-            printMessage('We have a tie!');
-        } else if (playerMove == 'unknown move' || computerMove == 'unknown move') {
-            printMessage('Something went wrong!');
-        } else {
-            printMessage('This time you loose :(');
-        }
-    }
-
     // Computers turn
     let randomNumber = getRandomNumber(1, 3);
-    // Math.floor(Math.random() * 3 + 1);  -- old version. Leaving for reference
+    // Math.floor(Math.random() * 3 + 1);  -- old version. Leaving for ref.
     let computerMove = getMoveName(randomNumber);
     // Players turn
     let playerMove = getMoveName(playerInput);
-
+    
     console.log('randomNumber: ' + randomNumber + '. computerMove: ' + computerMove);
     console.log('playerInput: ' + playerInput + '. playerMove: ' + playerMove);
-
+    
     displayResult(computerMove, playerMove);
+}
+
+const getMoveName = function (moveId) {
+    if (moveId == 1) {
+        return 'rock';
+    } else if (moveId == 2) {
+        return 'paper';
+    } else if (moveId == 3) {
+        return 'scissors';
+    } else {
+        printMessage('Move with id ' + moveId + ' is unknown.<br><br>');
+        return 'unknown move';
+    }
+};
+
+const displayResult = function(computerMove, playerMove) {
+    printMessage('Computer played: ' + computerMove + '<br> You played: ' + playerMove + '.' + '<br><br>');
+
+    if (computerMove == 'rock' && playerMove == 'paper'
+        || computerMove == 'paper' && playerMove == 'scissors'
+        || computerMove == 'scissors' && playerMove == 'rock') {
+        printMessage('You win!');
+    } else if (computerMove === playerMove) {
+        printMessage('We have a tie!');
+    } else if (playerMove == 'unknown move' || computerMove == 'unknown move') {
+        printMessage('Something went wrong!');
+    } else {
+        printMessage('This time you loose :(');
+    }
 }
 
 // Action!
